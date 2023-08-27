d04260d93ec6bf159a79f5099f3e6f7968008933
Vinicius <vini-luiz@hotmail.com>
2023-08-27 15:34:07 -0300

    componetizando a aplicação

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,22 @@
+    <script setup>
+        const props = defineProps(['tarefas'])
+    
+    </script>
+    
+    <template>
+            <ul class="list-group mt-4">
+                <span v-if="tarefas.length === 0">Não existem tarefas</span>
+                <li class="list-group-item" v-for="tarefa in tarefas" :key="tarefa.titulo">
+                    <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+                    <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                        {{ tarefa.titulo }}
+                    </label>
+                </li>
+        </ul>
+    </template>
+
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
